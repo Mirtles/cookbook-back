afd8c4159a1cea60b31740808e3ea1ee0ad394c3
Mirtles <mirte.post@yahoo.co.uk>
2019-10-09 14:40:05 +0200

    Add sample course data

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 
 const userRouter = require('./user/router')
 const loginRouter = require('./auth/router')
+const courseRouter = require('./course/router')
 
 const app = express()
 const port = 4000
@@ -16,6 +17,7 @@ app.use(
   parserMiddleware,
   userRouter,
   loginRouter,
+  courseRouter,
 )
 
 app.listen(port, () => console.log(`\nListening on port ${port}.\n`))
